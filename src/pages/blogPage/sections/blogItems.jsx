478982912a690457e3e238fb59105ec0d328e937
Wilbert Guo <wilbeguo@amazon.com>
2020-10-26 15:38:09 -0700

    Remove semicolons

--- src/pages/blogPage/sections/blogItems.jsx
+++ src/pages/blogPage/sections/blogItems.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import withStyles from "@material-ui/core/styles/withStyles"
-import BlogWidget from "../../../components/BlogWidget/BlogWidget";
+import BlogWidget from "../../../components/BlogWidget/BlogWidget"
 
 const styles = {
 };
