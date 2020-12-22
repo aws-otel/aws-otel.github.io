9e857bbb08b62d4de5499b138a6e1ba0ac39d0a5
Wilbert Guo <wilbeguo@amazon.com>
2020-12-22 10:46:02 -0800

    Remove font-family for "Roboto", "Helvetica", "Arial", sans-serif

--- src/assets/jss/material-kit-react.jsx
+++ src/assets/jss/material-kit-react.jsx
@@ -64,7 +64,7 @@ const card = {
 };
 
 const defaultFont = {
-  fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+//   fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
   fontWeight: "300",
   lineHeight: "1.5em"
 };
@@ -160,7 +160,7 @@ const title = {
   margin: "1.75rem 0 0.875rem",
   textDecoration: "none",
   fontWeight: "700",
-  fontFamily: `"Roboto Slab", "Times New Roman", serif`
+//   fontFamily: `"Roboto Slab", "Times New Roman", serif`
 };
 
 const cardTitle = {
