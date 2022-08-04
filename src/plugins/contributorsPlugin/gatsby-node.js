0526573d9f0c8fb903918a2de6e59aaf64d50af4
Raphael Silva <raphasil@amazon.com>
2022-08-03 17:23:56 -0700

    Chore: Allow to develop locally without a GitHub API key
    Fixes #57. A new environment variable NO_GH_API_KEY was introduced
    and is used as flag to show that we don't want to provide GitHub API keys

--- src/plugins/contributorsPlugin/gatsby-node.js
+++ src/plugins/contributorsPlugin/gatsby-node.js
@@ -23,15 +23,57 @@
 const GitHub = require("github-base")
 const camelcaseKeys = require("camelcase-keys")
 
+function octocatContributor() {
+    // Using example provided by the docs https://docs.github.com/en/rest/repos/repos#list-repository-contributors
+    const githubContributor = {
+        login: "octocat",
+        id: 1,
+        node_id: "MDQ6VXNlcjE=",
+        avatar_url: "https://github.githubassets.com/images/modules/logos_page/Octocat.png",
+        gravatar_id: "",
+        url: "https://github.com/octocat",
+        html_url: "https://github.com/octocat",
+        followers_url: "https://api.github.com/users/octocat/followers",
+        following_url: "https://api.github.com/users/octocat/following{/other_user}",
+        gists_url: "https://api.github.com/users/octocat/gists{/gist_id}",
+        starred_url: "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+        subscriptions_url: "https://api.github.com/users/octocat/subscriptions",
+        organizations_url: "https://api.github.com/users/octocat/orgs",
+        repos_url: "https://api.github.com/users/octocat/repos",
+        events_url: "https://api.github.com/users/octocat/events{/privacy}",
+        received_events_url: "https://api.github.com/users/octocat/received_events",
+        type: "User",
+        site_admin: false,
+        contributions: 32
+      };
+
+      return camelcaseKeys({...githubContributor, name: "octocat"})
+}
+
 exports.sourceNodes = async (
     { actions, createNodeId, createContentDigest },
     options
 ) => {
-    const github = new GitHub(options);
+
+    // Used in Development mode so that we don't try to hit the GitHub API
+    if (options.useMockData) {
+       actions.createNode({
+        ...octocatContributor(),
+        id: createNodeId(1),
+        internal: {
+            type: "GitHubContributor",
+            contentDigest: createContentDigest('1')
+        }
+      });
+
+      return
+    }
+
+    const github = new GitHub({token: options.token});
 
     const loginsSet = new Set()
 
-    const responses = await Promise.all(options.repos.map((repo) => github.paged(`/repos/:${repo}/contributors`, options, null)));
+    const responses = await Promise.all(options.repos.map((repo) => github.paged(`/repos/:${repo}/contributors`)));
 
     const contributors = responses.map((res) => res.pages.flat().map((page) => page.body).flat()).flat();
 
@@ -40,7 +82,7 @@ exports.sourceNodes = async (
 
     const profiles = await Promise.all(
         [...loginsSet].map(login =>
-            github.get(`/users/${login}`, options).then((res) => res.body)
+            github.get(`/users/${login}`).then((res) => res.body)
         )
     );
 
